b41a817c6900ca71be7e0c8e10e5da1720e50f1b
Adel Helal <3delhelal@gmail.com>
2025-11-10 21:48:42 +0200

    feat(home): inject Bloc into Material App

--- lib/todone_app.dart
+++ lib/todone_app.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import '/core/di.dart';
 import '/core/constants/app_constants.dart';
 import '/core/routing/routes.dart';
 import '/core/routing/app_router.dart';
 import '/core/localization/app_localizations.dart';
 import '/core/theming/theme_manager.dart';
+import 'features/home/presentation/controller/tasks_cubit.dart';
 
 class ToDoneApp extends StatelessWidget {
   const ToDoneApp({super.key});
@@ -18,19 +21,22 @@ class ToDoneApp extends StatelessWidget {
       designSize: const Size(392, 856),
       minTextAdapt: true,
       splitScreenMode: true,
-      builder: (_, child) {
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          title: AppConstants.appName,
-          initialRoute: RoutesManager.home,
-          onGenerateRoute: AppRouter.onGenerateRoute,
-          supportedLocales: AppLocalizations.supportedLocales,
-          localizationsDelegates: AppLocalizations.localizationsDelegates,
-          locale: Locale(AppConstants.englishLocaleCode),
-          themeMode: ThemeMode.system,
-          theme: ThemeManager.lightTheme,
-          darkTheme: ThemeManager.darkTheme,
-          themeAnimationDuration: Duration.zero,
+      builder: (_, _) {
+        return MultiBlocProvider(
+          providers: [BlocProvider<TasksCubit>(create: (_) => getIt())],
+          child: MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: AppConstants.appName,
+            initialRoute: RoutesManager.home,
+            onGenerateRoute: AppRouter.onGenerateRoute,
+            supportedLocales: AppLocalizations.supportedLocales,
+            localizationsDelegates: AppLocalizations.localizationsDelegates,
+            locale: Locale(AppConstants.englishLocaleCode),
+            themeMode: ThemeMode.system,
+            theme: ThemeManager.lightTheme,
+            darkTheme: ThemeManager.darkTheme,
+            themeAnimationDuration: Duration.zero,
+          ),
         );
       },
     );
