63e63bc6394cc7d7e8fdaf76de6e5df7f477fa87
Adel Helal <3delhelal@gmail.com>
2025-11-10 21:42:43 +0200

    feat(home): add responsive ui package to the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart';
 import '/core/di.dart';
@@ -11,6 +12,7 @@ void main() async {
   final appDocDir = await getApplicationDocumentsDirectory();
   Hive.init(appDocDir.path);
   setupDI();
+  await ScreenUtil.ensureScreenSize();
   runApp(const ToDoneApp());
 }
 
--- lib/todone_app.dart
+++ lib/todone_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '/core/constants/app_constants.dart';
 import '/core/routing/routes.dart';
 import '/core/routing/app_router.dart';
@@ -10,18 +11,28 @@ class ToDoneApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: AppConstants.appName,
-      initialRoute: RoutesManager.home,
-      onGenerateRoute: AppRouter.onGenerateRoute,
-      supportedLocales: AppLocalizations.supportedLocales,
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
-      locale: Locale(AppConstants.englishLocaleCode),
-      themeMode: ThemeMode.system,
-      theme: ThemeManager.lightTheme,
-      darkTheme: ThemeManager.darkTheme,
-      themeAnimationDuration: Duration.zero,
+    ScreenUtil.init(
+      context,
+    ); // This line to support font adaptation in the textTheme of app theme
+    return ScreenUtilInit(
+      designSize: const Size(392, 856),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (_, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: AppConstants.appName,
+          initialRoute: RoutesManager.home,
+          onGenerateRoute: AppRouter.onGenerateRoute,
+          supportedLocales: AppLocalizations.supportedLocales,
+          localizationsDelegates: AppLocalizations.localizationsDelegates,
+          locale: Locale(AppConstants.englishLocaleCode),
+          themeMode: ThemeMode.system,
+          theme: ThemeManager.lightTheme,
+          darkTheme: ThemeManager.darkTheme,
+          themeAnimationDuration: Duration.zero,
+        );
+      },
     );
   }
 }
