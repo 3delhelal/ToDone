012b62b505a5a9b061a79e3b73733f52a005427d
Adel Helal <3delhelal@gmail.com>
2025-11-12 18:59:33 +0200

    refactor(home): remove responsive ui package from the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart';
 import '/core/di.dart';
@@ -10,6 +9,5 @@ void main() async {
   final appDocDir = await getApplicationDocumentsDirectory();
   Hive.init(appDocDir.path);
   setupDI();
-  await ScreenUtil.ensureScreenSize();
   runApp(const ToDoneApp());
 }
--- lib/todone_app.dart
+++ lib/todone_app.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '/core/di.dart';
 import '/core/constants/app_constants.dart';
 import '/core/routing/routes.dart';
@@ -14,31 +13,21 @@ class ToDoneApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    ScreenUtil.init(
-      context,
-    ); // This line to support font adaptation in the textTheme of app theme
-    return ScreenUtilInit(
-      designSize: const Size(392, 856),
-      minTextAdapt: true,
-      splitScreenMode: true,
-      builder: (_, _) {
-        return MultiBlocProvider(
-          providers: [BlocProvider<TasksCubit>(create: (_) => getIt())],
-          child: MaterialApp(
-            debugShowCheckedModeBanner: false,
-            title: AppConstants.appName,
-            initialRoute: RoutesManager.home,
-            onGenerateRoute: AppRouter.onGenerateRoute,
-            supportedLocales: AppLocalizations.supportedLocales,
-            localizationsDelegates: AppLocalizations.localizationsDelegates,
-            locale: Locale(AppConstants.englishLocaleCode),
-            themeMode: ThemeMode.system,
-            theme: ThemeManager.lightTheme,
-            darkTheme: ThemeManager.darkTheme,
-            themeAnimationDuration: Duration.zero,
-          ),
-        );
-      },
+    return MultiBlocProvider(
+      providers: [BlocProvider<TasksCubit>(create: (_) => getIt())],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: AppConstants.appName,
+        initialRoute: RoutesManager.home,
+        onGenerateRoute: AppRouter.onGenerateRoute,
+        supportedLocales: AppLocalizations.supportedLocales,
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        locale: Locale(AppConstants.englishLocaleCode),
+        themeMode: ThemeMode.system,
+        theme: ThemeManager.lightTheme,
+        darkTheme: ThemeManager.darkTheme,
+        themeAnimationDuration: Duration.zero,
+      ),
     );
   }
 }
