b7476c39e8f23e339e93a013e457cc27acc1a79c
DESKTOP-590FC17\adelh <3delhelal@gmail.com>
2025-12-11 16:23:56 +0200

    fix(language): fix language entities path

--- lib/features/language/presentation/ui/change_language_screen.dart
+++ lib/features/language/presentation/ui/change_language_screen.dart
@@ -4,7 +4,7 @@ import '/core/constants/language_constants.dart';
 import '/core/helpers/extensions/context_extensions.dart';
 import '/core/widgets/spacing.dart';
 import '/features/language/presentation/controller/language_cubit.dart';
-import '../../domain/entity/language_entity.dart';
+import '../../domain/entities/language_entity.dart';
 import 'widgets/language_card.dart';
 
 class LanguageSettingsScreen extends StatelessWidget {
