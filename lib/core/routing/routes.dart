c1b5d355be58ccb1cc682dbb10f010944833a538
DESKTOP-590FC17\adelh <3delhelal@gmail.com>
2025-12-11 16:31:40 +0200

    feat(language):add language settings to app routes and add navigation to it from settings screen.

--- lib/core/routing/routes.dart
+++ lib/core/routing/routes.dart
@@ -4,4 +4,5 @@ class RoutesManager {
   static const String settings = 'settings';
   static const String themeSettings = 'theme-settings';
   static const String pomodoro = 'pomodoro';
+  static const String languageSettings = 'language';
 }
