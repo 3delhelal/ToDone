5678b92953d3e69ef6787ee2e6bb264af10a9faa
DESKTOP-590FC17\adelh <3delhelal@gmail.com>
2025-12-11 16:57:23 +0200

    feat(language): add the other 3 language to localization

---
+++ lib/core/localization/app_localizations_de.dart
@@ -0,0 +1,147 @@
+import '/core/constants/language_constants.dart';
+import 'app_localizations.dart';
+
+/// The translations for German (`de`).
+class AppLocalizationsDe extends AppLocalizations {
+  AppLocalizationsDe([super.locale = LanguageConstants.deLanguageCode]);
+
+  @override
+  String get welcomeMessage => 'Hallo Welt!';
+
+  // Home Feature
+  @override
+  String get tasksList => 'Aufgabenliste';
+  @override
+  String get unDone => 'Offen';
+  @override
+  String get done => 'Erledigt';
+  @override
+  String get somethingWentWrong =>
+      'Beim Abrufen der Aufgaben ist ein Fehler aufgetreten.';
+  @override
+  String get noTasksHere => 'Keine Aufgaben hier 👀.';
+  @override
+  String get confirmMessage => 'Bist du sicher?';
+  @override
+  String get confirmDeleteCardMsg => 'Die Aufgabe wird dauerhaft gelöscht.';
+
+  // Home Navigation Bar
+  @override
+  String get pomodoro => 'Pomodoro';
+  @override
+  String get settings => 'Einstellungen';
+
+  // Common Widgets
+  @override
+  String get cancel => 'Abbrechen';
+  @override
+  String get confirm => 'Bestätigen';
+
+  // Manage Task
+  @override
+  String get addTask => 'Aufgabe hinzufügen';
+  @override
+  String get editTask => 'Aufgabe bearbeiten';
+  @override
+  String get title => 'Titel';
+  @override
+  String get titleHint => 'Gib den Aufgabentitel ein';
+  @override
+  String get note => 'Notiz';
+  @override
+  String get noteHint => 'füge eine Notiz hinzu (optional)';
+  @override
+  String get priority => 'Priorität';
+  @override
+  String get lowPriority => 'Niedrig';
+  @override
+  String get meduimPriority => 'Mittel';
+  @override
+  String get highPriority => 'Hoch';
+  @override
+  String get time => 'Zeit';
+  @override
+  String get save => 'Speichern';
+
+  // Settings Screen
+  @override
+  String get themeSettings => 'Thema';
+  @override
+  String get languageSettings => 'Sprache';
+
+  // Theme Settings Screen
+  @override
+  String get manageThemeMessage => "Thema auswählen";
+  @override
+  get systemDefaultTheme => "Systemstandard";
+  @override
+  get lightTheme => "Helles Thema";
+  @override
+  get darkTheme => "Dunkles Thema";
+
+  // Pomodoro
+  @override
+  String get pomodoroTitle => "Pomodoro";
+  @override
+  String get chooseSessionTime => "Sitzungszeit auswählen";
+  @override
+  String get minute => "Minute";
+  @override
+  String get startSession => "Sitzung starten";
+  @override
+  String get pomodoroQuestion => "Was ist Pomodoro?";
+  @override
+  String get pomodoroAnswer =>
+      "Pomodoro ist eine Zeitmanagement-Technik, die dir hilft, konzentriert zu bleiben, indem du in kurzen Intervallen (Sitzungen) arbeitest, gefolgt von kurzen Pausen.";
+
+  @override
+  String get createTaskMsg => "Wähle eine Aufgabe zum Arbeiten";
+  @override
+  String get startWIthoutTaskMsg => "Ohne Aufgabe starten";
+  @override
+  String get withoutTask => "Ohne Aufgabe";
+  @override
+  String get enjoyBreak => "Genieße deine Pause!";
+  @override
+  String get focusTime => "Fokuszeit";
+  @override
+  String get breakTime => "Pausenzeit";
+  @override
+  String get endBreak => "Pause beenden";
+  @override
+  String get endSession => "Sitzung beenden";
+  @override
+  String get sessionComplete => "Sitzung abgeschlossen 👏";
+  @override
+  String get breakTimeMsg => "Zeit für eine Pause, um deinen Fokus aufzuladen.";
+
+  @override
+  String get startBreak => "Pause starten";
+  @override
+  String get cycleCompleted => "Zyklus abgeschlossen 🎉";
+
+  @override
+  String get finishedCycleMsg =>
+      "Du hast einen vollständigen Pomodoro-Zyklus abgeschlossen! Großartige Arbeit!";
+
+  @override
+  String get startNewCycle => "Neuen Zyklus starten";
+
+  // Language Feature
+  @override
+  String get selectLanguageMessage => "Sprache auswählen";
+  @override
+  String get unKnown => "Unbekannt";
+
+  // Languages
+  @override
+  String get ar => 'Arabisch';
+  @override
+  String get en => 'Englisch';
+  @override
+  String get es => 'Spanisch';
+  @override
+  String get de => 'Deutsch';
+  @override
+  String get fr => 'Französisch';
+}
---
+++ lib/core/localization/app_localizations_es.dart
@@ -0,0 +1,147 @@
+import '/core/constants/language_constants.dart';
+import 'app_localizations.dart';
+
+/// The translations for Spanish (`es`).
+class AppLocalizationsEs extends AppLocalizations {
+  AppLocalizationsEs([super.locale = LanguageConstants.esLanguageCode]);
+
+  @override
+  String get welcomeMessage => '¡Hola Mundo!';
+
+  // Home Feature
+  @override
+  String get tasksList => 'Lista de tareas';
+  @override
+  String get unDone => 'Por hacer';
+  @override
+  String get done => 'Completadas';
+  @override
+  String get somethingWentWrong => 'Algo salió mal al obtener las tareas.';
+  @override
+  String get noTasksHere => 'No hay tareas aquí 👀.';
+  @override
+  String get confirmMessage => '¿Estás seguro?';
+  @override
+  String get confirmDeleteCardMsg => 'La tarea se eliminará permanentemente.';
+
+  // Home Navigation Bar
+  @override
+  String get pomodoro => 'Pomodoro';
+  @override
+  String get settings => 'Ajustes';
+
+  // Common Widgets
+  @override
+  String get cancel => 'Cancelar';
+  @override
+  String get confirm => 'Confirmar';
+
+  // Manage Task
+  @override
+  String get addTask => 'Agregar tarea';
+  @override
+  String get editTask => 'Editar tarea';
+  @override
+  String get title => 'Título';
+  @override
+  String get titleHint => 'Ingresa el título de la tarea';
+  @override
+  String get note => 'Nota';
+  @override
+  String get noteHint => 'agrega una nota (opcional)';
+  @override
+  String get priority => 'Prioridad';
+  @override
+  String get lowPriority => 'Baja';
+  @override
+  String get meduimPriority => 'Media';
+  @override
+  String get highPriority => 'Alta';
+  @override
+  String get time => 'Tiempo';
+  @override
+  String get save => 'Guardar';
+
+  // Settings Screen
+  @override
+  String get themeSettings => 'Tema';
+  @override
+  String get languageSettings => 'Idioma';
+
+  // Theme Settings Screen
+  @override
+  String get manageThemeMessage => "Seleccionar tema";
+  @override
+  get systemDefaultTheme => "Por defecto del sistema";
+  @override
+  get lightTheme => "Tema claro";
+  @override
+  get darkTheme => "Tema oscuro";
+
+  // Pomodoro
+  @override
+  String get pomodoroTitle => "Pomodoro";
+  @override
+  String get chooseSessionTime => "Elige el tiempo de sesión";
+  @override
+  String get minute => "Minuto";
+  @override
+  String get startSession => "Iniciar sesión";
+  @override
+  String get pomodoroQuestion => "¿Qué es Pomodoro?";
+  @override
+  String get pomodoroAnswer =>
+      "Pomodoro es una técnica de gestión del tiempo que te ayuda a mantenerte enfocado trabajando en intervalos cortos (llamados sesiones) seguidos de pequeños descansos.";
+
+  @override
+  String get createTaskMsg => "Elige una tarea para trabajar";
+  @override
+  String get startWIthoutTaskMsg => "Comenzar sin tarea";
+  @override
+  String get withoutTask => "Sin tarea";
+  @override
+  String get enjoyBreak => "¡Disfruta tu descanso!";
+  @override
+  String get focusTime => "Tiempo de enfoque";
+  @override
+  String get breakTime => "Tiempo de descanso";
+  @override
+  String get endBreak => "Terminar descanso";
+  @override
+  String get endSession => "Terminar sesión";
+  @override
+  String get sessionComplete => "¡Sesión completada 👏!";
+  @override
+  String get breakTimeMsg =>
+      "Es momento de tomar un descanso y recargar tu enfoque.";
+
+  @override
+  String get startBreak => "Iniciar descanso";
+  @override
+  String get cycleCompleted => "Ciclo completado 🎉";
+
+  @override
+  String get finishedCycleMsg =>
+      "¡Has completado un ciclo completo de Pomodoro! ¡Gran trabajo manteniéndote enfocado!";
+
+  @override
+  String get startNewCycle => "Iniciar nuevo ciclo";
+
+  // Language Feature
+  @override
+  String get selectLanguageMessage => "Seleccionar idioma";
+  @override
+  String get unKnown => "Desconocido";
+
+  // Languages
+  @override
+  String get ar => 'Árabe';
+  @override
+  String get en => 'Inglés';
+  @override
+  String get es => 'Español';
+  @override
+  String get de => 'Alemán';
+  @override
+  String get fr => 'Francés';
+}
