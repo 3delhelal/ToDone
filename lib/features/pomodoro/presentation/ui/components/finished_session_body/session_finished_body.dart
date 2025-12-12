b6377149274418053b7dac238fe76d5202eaca11
DESKTOP-590FC17\adelh <3delhelal@gmail.com>
2025-12-12 22:38:00 +0200

    refactor(pomodoro): making session finished body support mobile and tablet layouts

--- lib/features/pomodoro/presentation/ui/components/finished_session_body/session_finished_body.dart
+++ lib/features/pomodoro/presentation/ui/components/finished_session_body/session_finished_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import '/core/widgets/responsive_builder.dart';
 import '/core/constants/app_constants.dart';
 import '/core/helpers/extensions/context_extensions.dart';
 import '/core/widgets/spacing.dart';
@@ -12,61 +13,150 @@ class FinishedSessionBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final PomodoroCubit pomodoroCubit = context.read<PomodoroCubit>();
     final size = MediaQuery.of(context).size;
-    // make ring size responsive but never larger than available width and never zero
-    final ringSize = (size.width * 0.65).clamp(120.0, size.width - 48.0);
+    final minSide = size.width < size.height ? size.width : size.height;
+    final ringSize = (minSide * AppConstants.pomodoroRingSizeRatio).clamp(
+      120.0,
+      size.width - 48.0,
+    );
 
-    return Center(
-      child: Column(
+    return ResponsiveBuilder(
+      mobile: _buildMobileLayout(context, ringSize, pomodoroCubit),
+      tablet: _buildTabletLayout(context, ringSize, pomodoroCubit),
+    );
+  }
+}
+
+Widget _buildMobileLayout(
+  BuildContext context,
+  double ringSize,
+  PomodoroCubit pomodoroCubit,
+) {
+  return Center(
+    child: Column(
+      children: [
+        HeaderContainer(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                context.localize.sessionComplete,
+                textAlign: TextAlign.center,
+                style: context.theme.textTheme.titleLarge,
+              ),
+              const VerticalSpace(10),
+              Text(
+                context.localize.breakTimeMsg,
+                textAlign: TextAlign.center,
+                style: context.theme.textTheme.labelLarge,
+              ),
+            ],
+          ),
+        ),
+        CircleMinutesPicker(
+          ringSize: ringSize,
+          lable: context.localize.minute,
+          initialIndex: (context.read<PomodoroCubit>().breakMinutes - 5) ~/ 5,
+          minValue: AppConstants.breakMinutePickerMinValue,
+          maxValue: AppConstants.breakMinutePickerMaxValue,
+          itemCount: AppConstants.breakMinutePickerItemCount,
+          onChange: (value) => pomodoroCubit.setBreakkMinutes(value),
+        ),
+        const VerticalSpace(40),
+        FilledButton(
+          onPressed: () {
+            pomodoroCubit.startBreak();
+          },
+          child: Text(context.localize.startBreak),
+        ),
+        const VerticalSpace(10),
+        OutlinedButton(
+          onPressed: () {
+            pomodoroCubit.cancelPomodoro();
+          },
+          child: Text(context.localize.cancel),
+        ),
+      ],
+    ),
+  );
+}
+
+Widget _buildTabletLayout(
+  BuildContext context,
+  double ringSize,
+  PomodoroCubit pomodoroCubit,
+) {
+  return Column(
+    children: [
+      Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           HeaderContainer(
+            isTabletLayout: true,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
+
               children: [
                 Text(
                   context.localize.sessionComplete,
                   textAlign: TextAlign.center,
-                  style: context.theme.textTheme.titleLarge,
+                  style: context.theme.textTheme.titleMedium,
                 ),
 
-                const VerticalSpace(10),
+                const VerticalSpace(3),
                 Text(
                   context.localize.breakTimeMsg,
                   textAlign: TextAlign.center,
-                  style: context.theme.textTheme.labelLarge,
+                  style: context.theme.textTheme.labelMedium,
                 ),
               ],
             ),
           ),
-
-          CircleMinutesPicker(
-            ringSize: ringSize,
-            lable: context.localize.minute,
-            initialIndex: (context.read<PomodoroCubit>().breakMinutes - 5) ~/ 5,
-            minValue: AppConstants.breakMinutePickerMinValue,
-            maxValue: AppConstants.breakMinutePickerMaxValue,
-            itemCount: AppConstants.breakMinutePickerItemCount,
-            onChange: (value) =>
-                context.read<PomodoroCubit>().setBreakkMinutes(value),
+        ],
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Expanded(
+            child: Column(
+              children: [
+                VerticalSpace(10),
+                CircleMinutesPicker(
+                  ringSize: ringSize,
+                  lable: context.localize.minute,
+                  initialIndex:
+                      (context.read<PomodoroCubit>().breakMinutes - 5) ~/ 5,
+                  minValue: AppConstants.sessionMinutePickerMinValue,
+                  maxValue: AppConstants.sessionMinutePickerMaxValue,
+                  itemCount: AppConstants.sessionMinutePickerItemCount,
+                  onChange: (value) => pomodoroCubit.setBreakkMinutes(value),
+                ),
+              ],
+            ),
           ),
-
-          const VerticalSpace(40),
-
-          FilledButton(
-            onPressed: () {
-              context.read<PomodoroCubit>().startBreak();
-            },
-            child: Text(context.localize.startBreak),
-          ),
-          const VerticalSpace(10),
-          OutlinedButton(
-            onPressed: () {
-              context.read<PomodoroCubit>().cancelPomodoro();
-            },
-            child: Text(context.localize.cancel),
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                FilledButton(
+                  onPressed: () {
+                    pomodoroCubit.startBreak();
+                  },
+                  child: Text(context.localize.startBreak),
+                ),
+                const VerticalSpace(10),
+                OutlinedButton(
+                  onPressed: () {
+                    pomodoroCubit.cancelPomodoro();
+                  },
+                  child: Text(context.localize.cancel),
+                ),
+              ],
+            ),
           ),
         ],
       ),
-    );
-  }
+    ],
+  );
 }
