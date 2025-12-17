62693891c3fb2841b7e25e85c336383617b90f32
DESKTOP-590FC17\adelh <3delhelal@gmail.com>
2025-12-17 23:43:18 +0200

    refactor(core): making app font sizes bigger size

--- lib/core/resources/font_styles_manager.dart
+++ lib/core/resources/font_styles_manager.dart
@@ -6,38 +6,38 @@ class TextStylesManager {
   //            >>>LIGHT THEME<<<
   // Title
   static TextStyle titleLargeLight = TextStyle(
-    fontSize: 17,
+    fontSize: 18,
     fontWeight: FontWeightsManager.bold,
     color: ColorsManager.black,
   );
 
   static TextStyle titleMediumLight = TextStyle(
-    fontSize: 16,
+    fontSize: 17,
     fontWeight: FontWeightsManager.semiBold,
     color: ColorsManager.black,
   );
 
   static TextStyle titleSmallLight = TextStyle(
-    fontSize: 15,
+    fontSize: 16,
     fontWeight: FontWeightsManager.medium,
     color: ColorsManager.black,
   );
   // Body
   // Light Theme Body
   static TextStyle bodyLargeLight = TextStyle(
-    fontSize: 15,
+    fontSize: 16,
     fontWeight: FontWeightsManager.medium,
     color: ColorsManager.black,
   );
 
   static TextStyle bodyMediumLight = TextStyle(
-    fontSize: 13,
+    fontSize: 14,
     fontWeight: FontWeightsManager.regular,
     color: ColorsManager.black,
   );
 
   static TextStyle bodySmallLight = TextStyle(
-    fontSize: 11,
+    fontSize: 13,
     fontWeight: FontWeightsManager.regular,
     color: ColorsManager.black,
   );
@@ -49,50 +49,50 @@ class TextStylesManager {
   );
 
   static TextStyle captionMediumLight = TextStyle(
-    fontSize: 12,
+    fontSize: 13,
     fontWeight: FontWeightsManager.regular,
     color: ColorsManager.grey,
   );
 
   static TextStyle captionSmallLight = TextStyle(
-    fontSize: 10,
+    fontSize: 12,
     fontWeight: FontWeightsManager.regular,
     color: ColorsManager.grey,
   );
   //        >>>DARK THEME<<<
   // Title
   static TextStyle titleLargeDark = TextStyle(
-    fontSize: 17,
+    fontSize: 18,
     fontWeight: FontWeightsManager.bold,
     color: ColorsManager.white,
   );
 
   static TextStyle titleMediumDark = TextStyle(
-    fontSize: 16,
+    fontSize: 17,
     fontWeight: FontWeightsManager.semiBold,
     color: ColorsManager.white,
   );
 
   static TextStyle titleSmallDark = TextStyle(
-    fontSize: 15,
+    fontSize: 16,
     fontWeight: FontWeightsManager.medium,
     color: ColorsManager.white,
   );
   // Body
   static TextStyle bodyLargeDark = TextStyle(
-    fontSize: 15,
+    fontSize: 16,
     fontWeight: FontWeightsManager.medium,
     color: ColorsManager.white,
   );
 
   static TextStyle bodyMediumDark = TextStyle(
-    fontSize: 13,
+    fontSize: 14,
     fontWeight: FontWeightsManager.regular,
     color: ColorsManager.white,
   );
 
   static TextStyle bodySmallDark = TextStyle(
-    fontSize: 11,
+    fontSize: 13,
     fontWeight: FontWeightsManager.regular,
     color: ColorsManager.white,
   );
@@ -104,13 +104,13 @@ class TextStylesManager {
   );
 
   static TextStyle captionMediumDark = TextStyle(
-    fontSize: 12,
+    fontSize: 13,
     fontWeight: FontWeightsManager.regular,
     color: ColorsManager.grey,
   );
 
   static TextStyle captionSmallDark = TextStyle(
-    fontSize: 10,
+    fontSize: 12,
     fontWeight: FontWeightsManager.regular,
     color: ColorsManager.grey,
   );
